1baf20c5d57f31081b51c29c58f2fc6682d736e3
andre-alck <andrealck1@gmail.com>
2022-11-01 01:20:45 -0300

    feat: add CreatePage structure

--- lib/create/create_page.dart
+++ lib/create/create_page.dart
@@ -9,8 +9,70 @@ class CreatePage extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
+    var titleController = TextEditingController();
+    var descriptionController = TextEditingController();
+    var priceController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(
+            8.0,
+          ),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blue.shade100,
+                  suffixIcon: const Icon(
+                    Icons.title,
+                  ),
+                  labelText: "Título",
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blue.shade100,
+                  suffixIcon: const Icon(
+                    Icons.description,
+                  ),
+                  labelText: "Descrição",
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: priceController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blue.shade100,
+                  suffixIcon: const Icon(
+                    Icons.price_change,
+                  ),
+                  labelText: "Preço",
+                ),
+              ),
+              Center(
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.publish,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
